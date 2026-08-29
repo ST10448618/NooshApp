c6d2a113e73bcd3263c1f19a80c2142c8bb949bc
Perezr12 <ST10448618@vcconnect.edu.za>
2026-08-29 20:10:14 +0200

    Login Issue Fix

---
+++ NooshApp.Web/Data/DataProtectionKeysContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NooshApp.Web.Data
+{
+    public class DataProtectionKeysContext : DbContext, IDataProtectionKeyContext
+    {
+        public DataProtectionKeysContext(DbContextOptions<DataProtectionKeysContext> options) : base(options) { }
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; } = default!;
+    }
+}
--- NooshApp.Web/Program.cs
+++ NooshApp.Web/Program.cs
@@ -1,4 +1,7 @@
 using NooshApp.Web.Services;
+using NooshApp.Web.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -47,7 +50,21 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddDbContext<DataProtectionKeysContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("KeysConnection")
+        ?? "Data Source=DataProtectionKeys.db"));
+
+builder.Services.AddDataProtection()
+    .PersistKeysToDbContext<DataProtectionKeysContext>()
+    .SetApplicationName("NooshApp"); 
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var keysDb = scope.ServiceProvider.GetRequiredService<DataProtectionKeysContext>();
+    keysDb.Database.EnsureCreated();
+}
 
 if (!app.Environment.IsDevelopment())
 {
